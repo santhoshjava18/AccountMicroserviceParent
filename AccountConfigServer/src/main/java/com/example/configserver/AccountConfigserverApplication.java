a2256c36668d6978ec9930befeab39f9b8eaf60e
santhosh <santhosh.java18@gmail.com>
2017-12-10 00:16:50 -0500

    Adding all services code

--- AccountConfigServer/src/main/java/com/example/configserver/AccountConfigserverApplication.java
+++ AccountConfigServer/src/main/java/com/example/configserver/AccountConfigserverApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class AccountConfigserverApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccountConfigserverApplication.class, args);
 	}
